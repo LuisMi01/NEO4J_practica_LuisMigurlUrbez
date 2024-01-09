277fe76239e7d0de597e67b4637f416ccaeb252e
The Chief <lurbevil@myuax.com>
2024-01-09 15:27:01 +0100

    Añadidas las imagenes y el banner

---
+++ web-react-ts/src/components/Banner.tsx
@@ -0,0 +1,50 @@
+import '../App.css';
+import githubLogo from '../';
+import uaxLogo from '../images/uaxlogo.png';
+
+function Banner() {
+  
+  return (
+    <div className="grid grid-cols-3 grid-rows-1 p-5 bg-slate-gray">
+      <div className="flex row gap-3">
+        <div onClick={() => {
+          window.open('https://github.com/LuisMi01/airbnb-mongodb_LuisMiguelUrbez', '_blank');
+        }
+        }
+             className="hidden md:block text-sm font-semibold py-0.5 px-0.5 rounded-full hover:bg-neutral-100 transition cursor-pointer ">
+          <img className="rounded-full object-cover" src={githubLogo} alt={"Github"} width={50} height={50} />
+        </div>
+        <div onClick={() => {
+          window.open('https://uax.com', '_blank');
+        }
+        }
+             className="hidden md:block text-sm font-semibold py-0.5 px-0.5 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          <img className="rounded-full object-cover" src={uaxLogo} alt={"Uax"} width={50} height={50} />
+        </div>
+      </div>
+
+
+      <div className="flex items-center justify-center ">
+        <form className="relative" onSubmit={manejarEnvio}>
+          <input value={busqueda} onChange={manejarCambio}  type="text" className="h-10 pl-8 pr-20 rounded-full z-0 focus:shadow focus:outline-none"
+                 placeholder="Busca un libro..." />
+          <div className="absolute top-2 right-2">
+            <button type='submit' className="h-6 w-6 text-gray-500">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
+
+
+      <div className="flex justify-end">
+       Menu
+    </div>
+    </div>
+  );
+};
+
+export default Banner;
