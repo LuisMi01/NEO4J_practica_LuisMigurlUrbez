39c98de8c6e9013a451611a9f7e2d54e359c8034
The Chief <lurbevil@myuax.com>
2024-01-09 14:56:56 +0100

    reinportado node en la seccion de la api

--- api/babel.config.js
+++ api/babel.config.js
@@ -16,6 +16,8 @@ module.exports = {
     ],
   ],
   plugins: [
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
     [
       'babel-plugin-module-resolver',
       {
