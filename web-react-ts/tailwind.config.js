2ff37587c9d56f4a0de3245191ad5df58ee134fa
The Chief <lurbevil@myuax.com>
2024-01-09 13:10:52 +0100

    Instalado tailwind CSS

---
+++ web-react-ts/tailwind.config.js
@@ -0,0 +1,19 @@
+
+const config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cool-gray': '#a7a5c6',
+        'cool-gray-2': '#8797b2',
+        'slate-gray': '#6d8a96',
+        'paynes-gray': '#5d707f',
+        'robin-egg-blue': '#66ced6',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
