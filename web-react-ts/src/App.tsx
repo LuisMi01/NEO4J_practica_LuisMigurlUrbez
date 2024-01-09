a4a2aed2a16fd4b64fecd80be3df588018a55387
The Chief <lurbevil@myuax.com>
2024-01-09 15:41:52 +0100

    Añadidas las primeras partes del banner

--- web-react-ts/src/App.tsx
+++ web-react-ts/src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import './App.css'
+'use client'
+import './App.css';
+import Banner from './components/Banner';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+
+// Crea un cliente Apollo
+const client = new ApolloClient({
+  uri: 'http://localhost:4001/graphql', //esto por ahora funciona en local, si quiero usarlo en server hay que cambiarlo
+  cache: new InMemoryCache()
+});
+
 
 function App() {
-  return <div>hola jefe</div>
+  
+  return (
+    <div className="bg-robin-egg-blue min-h-screen">
+      <ApolloProvider client={client}>
+        <Router>
+          <Banner />
+          <hr/>
+          <Routes>
+            <Route /> 
+          </Routes>
+        </Router>
+      </ApolloProvider>
+    </div>
+  );
 }
 
 export default App
--- web-react-ts/src/components/Banner.tsx
+++ web-react-ts/src/components/Banner.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import '../App.css';
-import githubLogo from '../';
+import githubLogo from '../images/githublogo.png';
 import uaxLogo from '../images/uaxlogo.png';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { BiMenuAltRight } from 'react-icons/bi';
+
+
 
 function Banner() {
   
@@ -8,7 +14,7 @@ function Banner() {
     <div className="grid grid-cols-3 grid-rows-1 p-5 bg-slate-gray">
       <div className="flex row gap-3">
         <div onClick={() => {
-          window.open('https://github.com/LuisMi01/airbnb-mongodb_LuisMiguelUrbez', '_blank');
+          window.open('https://github.com/LuisMi01/NEO4J_practica_LuisMigurlUrbez', '_blank');
         }
         }
              className="hidden md:block text-sm font-semibold py-0.5 px-0.5 rounded-full hover:bg-neutral-100 transition cursor-pointer ">
@@ -25,23 +31,22 @@ function Banner() {
 
 
       <div className="flex items-center justify-center ">
-        <form className="relative" onSubmit={manejarEnvio}>
-          <input value={busqueda} onChange={manejarCambio}  type="text" className="h-10 pl-8 pr-20 rounded-full z-0 focus:shadow focus:outline-none"
-                 placeholder="Busca un libro..." />
-          <div className="absolute top-2 right-2">
-            <button type='submit' className="h-6 w-6 text-gray-500">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-              </svg>
-            </button>
-          </div>
-        </form>
+        Marvel Universe
       </div>
 
 
       <div className="flex justify-end">
-       Menu
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <BiMenuAltRight size={30} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/buscar">Buscar</Dropdown.Item>
+                <Dropdown.Item href="#/comics">Comics</Dropdown.Item>
+                <Dropdown.Item href="#/escenas">Escenas</Dropdown.Item>
+                <Dropdown.Item href="#/personajes">Personajes</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
     </div>
     </div>
   );
